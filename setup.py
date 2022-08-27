e4c99ca9a3f52777aa9d23bed23034525aa8392b
Marek Kolman <marek.kolman@gmail.com>
2022-08-27 15:07:50 +0200

    ininital commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(
+    name='ql_extension',
+    version='0.0.1',    
+    description='A example Python package',
+    #url='https://github.com/shuds13/pyexample',
+    author='Marek Kolman',
+    author_email='marek.kolman@gmail.com',
+    license='MIT',
+    packages=['pyexample'],
+    install_requires=['QuantLib',
+                      'pandas',                     
+                      ],
+)
