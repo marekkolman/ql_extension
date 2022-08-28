04716368eb1b0077df3a0850162c5cfae6025729
Marek Kolman <marek.kolman@gmail.com>
2022-08-28 09:21:17 +0200

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ql_extension',
-    version='0.0.2',    
+    version='0.0.3',    
     description='A example Python package',
     #url='https://github.com/shuds13/pyexample',
     author='Marek Kolman',
